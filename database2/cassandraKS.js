f47e97ee0421a42c059f65b49d17faee7dfc44d9
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-17 13:41:57 -0500

    clean up file

--- database2/cassandraKS.js
+++ database2/cassandraKS.js
@@ -6,31 +6,6 @@ const client = new cass.Client({
   keyspace: 'system'
 });
 
-const dropKs = () => {
-  client.connect()
-    .then(() => {
-      const query = "DROP KEYSPACE products";
-      return client.execute(query)
-    })
-    .then((result) => console.log('keyspace dropped: ', result))
-    .catch((err) => {
-      console.log('Heres the problem: ', err)
-      return client.shutdown().then(() => { throw err; });
-    })
-}
-const createKs = () => {
-  client.connect()
-    .then(() => {
-      const query = "CREATE KEYSPACE IF NOT EXISTS products WITH replication =" + "{'class': 'SimpleStrategy', 'replication_factor': '1'}";
-      return client.execute(query);
-    })
-    .then((result) => console.log('keyspace created: ', result))
-    .catch((err) => {
-      console.error('Heres the problem: ', err);
-      return client.shutdown().then(() => { throw err; });
-    })
-}
-
 const createProductsKeyspace = () => {
   client.connect()
     .then(() => {
@@ -60,11 +35,5 @@ const createProductsKeyspace = () => {
       return client.shutdown().then(() => { throw err; });
     })
 }
-//  dropKs();
-//createKs();
+
 createProductsKeyspace();
-
-// const query = 'SELECT name, email FROM users WHERE key = ?';
-
-// client.execute(query, [ 'someone' ])
-//   .then(result => console.log('User with email %s', result.rows[0].email));
