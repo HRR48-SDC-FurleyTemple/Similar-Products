37c49db0a20a25f4454603996d32e5bddc3c4a20
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-17 13:40:28 -0500

    add create table to cass seed

--- database2/cassandraKS.js
+++ database2/cassandraKS.js
@@ -34,18 +34,27 @@ const createKs = () => {
 const createProductsKeyspace = () => {
   client.connect()
     .then(() => {
-      const query = "DROP KEYSPACE products";
+      const query = "DROP KEYSPACE similar_prod";
       return client.execute(query);
+    })
+    .catch((err) => {
+      console.error('keyspace not defined or other error: ', err);
     })
     .then((result) => {
       console.log('keyspace dropped: ', result);
-      const query = "CREATE KEYSPACE IF NOT EXISTS products WITH replication =" + "{'class': 'SimpleStrategy', 'replication_factor': '1'}";
+      const query = "CREATE KEYSPACE IF NOT EXISTS similar_prod WITH replication =" + "{'class': 'SimpleStrategy', 'replication_factor': '1'}";
       return client.execute(query);
     })
     .then((result) => {
       console.log('keyspace created: ', result);
+      const query = "CREATE TABLE IF NOT EXISTS similar_prod.products" + "(productId int PRIMARY KEY, name text, category text, price decimal, rating decimal, imageUrl text, onSale boolean)"
+      return client.execute(query);
+    })
+    .then((result) => {
+      console.log('table created: ', result);
       return client.shutdown().then(() => {console.log('connection complete')})
     })
+
     .catch((err) => {
       console.error('Heres the problem: ', err);
       return client.shutdown().then(() => { throw err; });
@@ -54,6 +63,7 @@ const createProductsKeyspace = () => {
 //  dropKs();
 //createKs();
 createProductsKeyspace();
+
 // const query = 'SELECT name, email FROM users WHERE key = ?';
 
 // client.execute(query, [ 'someone' ])
