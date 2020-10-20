23ef0f8c87b192cbf52aa1ed241741744d110ff1
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-19 19:00:07 -0500

    modify data generator

--- database1/dataGenerator2.js
+++ database1/dataGenerator2.js
@@ -6,20 +6,20 @@ const photoLibrarySize = 101;
 let dataWriter = fs.createWriteStream(`data/primary-records.csv`);
 
 dataWriter.write('productId, name, category, price, rating, imageUrl, onSale');
-
-const max = 14000000;
+//Leaves 10000 ids open per category to avoid having to shift all the product ids when a new one is added
+const max = 14060000;
 
 var i = 1;
 
 //Creates 14 mill records, 2 mill for each category
 //INDEXES/PRODUCT ID's
-    //'living-room': 1 - 2000000,
-    // 'kitchen': 2000001 - 4000000,
-    // 'dining'4000001 - 6000000,
-    // 'bedroom' 6000001 - 8000000,
-    // 'bathroom' 8000001 - 10000000,
-    // 'closet': 10000001 - 12000000,
-    // 'laundry': 12000001 - 14000000
+    //'living-room': 1 - 2010000,
+    // 'kitchen': 2010001 - 4020000,
+    // 'dining'4020001 - 6030000,
+    // 'bedroom' 6030001 - 804000,
+    // 'bathroom' 8040001 - 10050000,
+    // 'closet': 10050001 - 12060000,
+    // 'laundry': 12060001 - 14070000
 
 
 const createProductEntries = () => {
@@ -27,17 +27,17 @@ const createProductEntries = () => {
   var photoIndex = 1
 
   const setCategory = (i) => {
-    if ( i > 12000000) {
+    if ( i > 12060000) {
       return 'laundry';
-    } else if (i > 10000000) {
+    } else if (i > 10050000) {
       return 'closet';
-    } else if (i > 8000000) {
+    } else if (i > 8040000) {
       return 'bathroom';
-    } else if (i > 6000000) {
+    } else if (i > 6030000) {
       return 'bedroom';
-    } else if (i > 4000000) {
+    } else if (i > 4020000) {
       return 'dining';
-    } else if (i > 2000000) {
+    } else if (i > 2010000) {
       return 'kitchen';
     } else {
       return 'living';
@@ -51,7 +51,8 @@ const createProductEntries = () => {
       price: faker.random.number({min: 20, max: 1000}),
       rating: faker.random.number({min: 1, max: 5, precision: 0.5}),
       imageUrl: `https://cem-sdc.s3.us-east-2.amazonaws.com/ph0t0s/furniture${photoIndex}.jpg`,
-      onSale: faker.random.boolean(),
+      //onSale: faker.random.boolean(),
+      onSale: faker.random.number({min: 0, max: 1}),
       category: setCategory(i)
     }
     if (photoIndex === photoLibrarySize) {
@@ -59,8 +60,14 @@ const createProductEntries = () => {
     } else {
       photoIndex += 1;
     }
-    let furnitureString = `\n${values.productId}, ${values.name}, ${values.category}, ${values.price}, ${values.rating}, ${values.imageUrl}, ${values.onSale}`
-    i++;
+    let furnitureString = `\n${values.productId}, ${values.name}, ${values.category}, ${values.price}, ${values.rating}, ${values.imageUrl}, ${values.onSale}`;
+
+    if (i === 2000000 || i === 4010000 || i === 6020000 || i === 8030000 || i === 10040000 || i === 12050000) {
+      i += 10001
+    } else {
+      i++;
+    }
+
     if (i === max) {
       dataWriter.write(furnitureString);
     } else {
